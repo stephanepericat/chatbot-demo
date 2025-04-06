161f1b016777a3f5207329c9946b324c254a44e9
Stephane P. Pericat <contact@pericat.net>
2025-04-06 15:53:20 -0500

    feat(bot): form ui

---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+const llms = ref([
+  {type: 'label', label: 'AI Model'},
+  {label: 'Deepseek/Deepseek R1', value: 'deepseek/deepseek-r1:free', selected: true },
+  {label: 'Deepseek/Deepseek V3', value: 'deepseek/deepseek-chat:free' },
+  {label: 'Qwen/QWQ 32b', value: 'qwen/qwq-32b:free' },
+  {label: 'Qwen/Qwen 2.5 72b instruct', value: 'qwen/qwen-2.5-72b-instruct:free' },
+  {label: 'Qwen/Qwen VL 72b instruct', value: 'qwen/qwen2.5-vl-72b-instruct:free' },
+  {label: 'OpenRouter/Quasar alpha', value: 'openrouter/quasar-alpha' },
+  {label: 'MistralAI/Mistral 3.1 small', value: 'meta-llama/llama-4-maverick:free' },
+  {label: 'Meta/Llama 4.0 maverick', value: 'meta-llama/llama-4-maverick:free' },
+  {label: 'Meta/Llama 3.3 70b instruct', value: 'meta-llama/llama-3.3-70b-instruct:free' },
+  {label: 'Microsoft/Phi3 medium', value: 'microsoft/phi-3-medium-128k-instruct:free' },
+])
+
+const temps = ref([{type: 'label', label: 'Temperature'}, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0])
+
+const model = ref('deepseek/deepseek-r1:free')
+const question = ref('')
+const temperature = ref(0.5)
+
+const onSubmit = (e: Event) => {
+  e.preventDefault()
+  console.log('question:', question.value)
+  console.log('model:', model.value)
+  console.log('temperature:', temperature.value)
+}
+</script>
+
+<template>
+  <div class="mx-auto py-8 px-4 max-w-[48rem] h-[100vh]">
+    <div class="flex flex-col h-full gap-8">
+      <h1 class="text-3xl font-bold">Chatbot demo</h1>
+      <div class="grow-1 overflow-auto">
+        <div class="user">What can we expect will happen with respect to covid ten years from now?</div>
+        <div class="ai">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante ex, viverra quis aliquam a, luctus nec dolor. Etiam ligula felis, vehicula nec pellentesque sit amet, ornare ut odio. Suspendisse sed porta lectus. Nam pulvinar congue elit at vulputate. Nunc id lorem est. Sed rutrum quis sem in blandit. Etiam tempor finibus sapien, placerat venenatis magna sagittis ac. Nullam eget malesuada justo. Aenean accumsan pulvinar ante, id tincidunt magna fringilla sit amet. Vivamus urna nunc, tempus et ornare sed, cursus quis felis. Aliquam eu leo tortor. Donec consequat sit amet eros eu aliquet. Suspendisse lacinia, neque viverra fringilla luctus, nunc nulla consequat orci, at volutpat sapien justo in purus. Nam lectus est, ultrices id vestibulum vitae, bibendum quis nisi.
+        </div>
+      </div>
+      <div class="shrink-0 min-h-[150px]">
+        <form class="w-full" @submit="onSubmit">
+          <div class="w-full flex gap-4 items-center">
+            <UTextarea v-model="question" class="grow-1" :maxrows="4" autoresize color="neutral" variant="subtle" placeholder="Ask a question about covid-19..." />
+            <UButton type="submit" class="shrink-0 hover:cursor-pointer">Send</UButton>
+          </div>
+          <div class="mt-4 w-full flex justify-between">
+            <USelect v-model="model" class="w-1/3" :items="llms" placeholder="Choose a LLM..." />
+            <USelect v-model="temperature" class="w-1/3" :items="temps" placeholder="Choose a temperature..." />
+          </div>
+        </form>
+      </div>
+    </div>  </div>
+</template>
